5cd3982b78366fe8e1a28aeb086d46bccad4a586
Halimah22 <halimamohamed95@yahoo.com>
2019-06-21 05:41:48 -0500

    redux update

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+//Define action creator and dispatch after response from api
+const fetchUser = ( )=> {
+    return function(dispatch) {
+        axios.get('/api/current_user')
+        .then(res => dispatch({ type: FETCH_USER, payload: res }));
+    };
+};
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -6,19 +6,25 @@ const Dasbboard = () => <h2> Dasbboard</h2>;
 const SurveyNew = () => <h2> SurveyNew</h2>;
 const Landing = () => <h2> Landing</h2>;
 
-const App = () => {
-    return (
-        <div className="container">
-            <Header/>
-           <BrowserRouter>
-            <div>
-                <Route exact path="/" component={Landing}/>
-                <Route exact path="/surveys" component={Dasbboard}/>
-                <Route path="/surveys/new" component={SurveyNew}/>
+class App extends Component{
+    componentDidMount(){
+        
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Header/>
+                <BrowserRouter>
+                    <div>
+                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/surveys" component={Dasbboard}/>
+                    <Route path="/surveys/new" component={SurveyNew}/>
+                </div>
+            </BrowserRouter>     
             </div>
-           </BrowserRouter>     
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
