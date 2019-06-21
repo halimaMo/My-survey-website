1637050f5db99d5fbe096fe6f3087427bebe827f
Halimah22 <halimamohamed95@yahoo.com>
2019-06-21 07:39:49 -0500

    refrac action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-//Define action creator and dispatch after response from api
-export const fetchUser = ( )=> {
-    return function(dispatch) {
-        axios.get('/api/current_user')
-        .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//Define action creator and dispatch after response is completed from api
+export const fetchUser = () => async  dispatch => {
+        const res = await axios.get('/api/current_user');
+        
+        dispatch({ type: FETCH_USER, payload: res });
     };
-};
