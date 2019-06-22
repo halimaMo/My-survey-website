6445fb3902e813e6a6b3a7e444453a11f9496db3
Halimah22 <halimamohamed95@yahoo.com>
2019-06-22 13:37:50 -0500

    usermodel

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-//Define action creator and dispatch after response is completed from api
+//Define action creator and dispatch action after response is completed from api
 export const fetchUser = () => async  dispatch => {
-        const res = await axios.get('/api/current_user');
-        
-        dispatch({ type: FETCH_USER, payload: res });
+    // res is the request made to the backend server    
+    const res = await axios.get('/api/current_user');
+
+        dispatch({ type: FETCH_USER, payload: res.data });
     };
