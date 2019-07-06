336d8d044399584cec5447462da9acffcece8005
Halimah22 <halimamohamed95@yahoo.com>
2019-07-06 06:47:10 -0500

    Logout Link tag

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,13 +14,13 @@ module.exports = app => {
             }
         );
     
-    app.get('/api/curr  ent_user', (req,res) => {
+    app.get('/api/current_user', (req,res) => {
         res.send(req.user);
     });
 
     app.get('/api/logout', (req, res) => {
         req.logout();
-        res.send(req.user);
+        res.redirect('/');
     });
 };
 
