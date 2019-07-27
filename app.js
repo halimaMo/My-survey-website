47f6ef0595d3c5b84711c9a7012623e41fae24a3
Halimah22 <halimamohamed95@yahoo.com>
2019-07-27 06:23:05 -0500

    modelSetup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const keys =  require('./config/keys');
 mongoose.connect(keys.mongoURI);
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 const app = express();
