0a60d7e3d0d4a5cc00c93e876bee39765bd80390
Halimah22 <halimamohamed95@yahoo.com>
2019-08-01 13:20:49 -0500

    updated

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
     //Make sure Express w ill serve up procuction assets
