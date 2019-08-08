04fa422bafb457fe36f00c49862308aef3b3aa4f
Halimah22 <halimamohamed95@yahoo.com>
2019-08-08 16:41:42 -0500

    sendgrid

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,34 @@ const helper = sendgrid.mail;
 const keys =require('../config/keys');
 
 class Mailer extends helper.Mail{
+    constructor({ subject, recipients}, content) {
+        super();
+        
+        //sendgrid setup
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAdresses(recipients);
 
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
+
+    }
+formatAdresses(recipients) {
+    //destructuring
+    return recipients.map(({ email }) => {
+        return new helper.Email(email);
+    });
+ }
+}
+
+addClickTracking(){
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.clickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addClickTrackingSettings(trackingSettings);
 }
 
 module.exports = Mailer;
